d29bd32245334d7e926dc376885f954115e9e28c
aneyeforsecrets <me@dennis-irrgang.com>
2019-03-20 10:50:49 +0100

    Added documentation and logging capabilities - untested!

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import time
-import adafruit_sgp30
-import dht
+import adafruit_sgp30   # Library for the usage of the SGP30 VOC sensor
+import dht              # Library for the usage of the DHT22 temperature/humidity sensor
 import json
 import os
-
 from machine import Pin, I2C
+import logging
+import logging.handlers as handlers
+
+# Some Settings
+
+co2eq_warning = 1500    # co2eq level in ppm after which the warning is triggered
+
+# Set up logging
+logger = logging.getLogger('aqs')
+logger.setLevel(logging.INFO)
+
+# Here we define our formatter
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logHandler = handlers.RotatingFileHandler(
+    'app.log', maxBytes=256, backupCount=2)
+
+logHandler.setLevel(logging.INFO)
+
+# Here we set our logHandler's formatter
+logHandler.setFormatter(formatter)
+
+logger.addHandler(logHandler)
+
 
 # construct an I2C bus
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
@@ -12,43 +36,58 @@ i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
 # Create library object on our I2C port
 sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 
-# Initiialize SGP30 sensor and set baseline variables
+# Initiialize SGP30 sensor and restore baseline variables
+logging.info(
+    "Initiating SGP30 sensor and attempting to restore baseline values.")
+
 sgp30.iaq_init()
-
-# Create temperature sensor object
-dht = dht.DHT22(Pin(0))
-
-led_onboard = Pin(5, Pin.OUT)
-led_onboard.value(1)
-
-elapsed_sec = 0
-
-
-# Restore baseline files from flash
 with open("data_file.json") as infile:
     baselines = json.load(infile)
 
 sgp30.set_iaq_baseline(baselines["co2eq_base"], baselines["tvoc_base"])
+logging.info("Restored baseline values co2eq %d and tvoc %d",
+             baselines["co2eq_base"], baselines["tvoc_base"])
+
+# Create temperature sensor object
+dht = dht.DHT22(Pin(0))
+
+# Create LED object for on-board LED
+led_onboard = Pin(5, Pin.OUT)
+led_onboard.value(1)
+
+
+elapsed_sec = 0
 
 while True:
+    # Before the DHT22 values can be used measure() needs to be called seperately - limited to once every 2 seconds!
     dht.measure()
 
-    log = {"temperature" : dht.temperature(), "humidity" : dht.humidity(), "co2eq" : sgp30.co2eq, "tvoc" : sgp30.tvoc}
+    # Call values of the sensors and write them into a dictionary to allow storage as JSON later.
+    # SGP30 does not need a separate measure call
+    log = {"temperature": dht.temperature(), "humidity": dht.humidity(),
+           "co2eq": sgp30.co2eq, "tvoc": sgp30.tvoc}
 
-    with open ("history.json", "a+") as outfile:
+    # Append dictionary log as JSON
+    with open("history.json", "a+") as outfile:
         json.dump(log, outfile)
 
-    if co2eq >= 1500 and led_onboard.value() != 0:
+    # If CO2eq levels exceed 1500 ppm, turn on LED to signal it's time to crack open a window
+    if log["co2eq"] >= co2eq_warning and led_onboard.value() != 0:
+        logging.info("CO2eq level exceeded %d ppm!", co2eq_warning)
         led_onboard.value(0)
     elif led_onboard.value() != 1:
+        logging.info("CO2eq back below %d ppm.", co2eq_warning)
         led_onboard.value(1)
 
     print("temp = {:5.1f} °C \t humi = {:5.1f}% \t CO2e = {:d} ppm \t TVOC = {:d} ppb".format(
         log["temperature"], log["humidity"], log["co2eq"], log["tvoc"]))
 
+    # Wait - minimum 2 seconds before DHT22 sensor can be measured again
     time.sleep(2)
     elapsed_sec += 2
 
+    # According to the sensor documentation of the SGP30 it automatically calibrates itself
+    # depending on the environment. These values should be stored once every hour and restored after boot
     if elapsed_sec % 3600 == 0:
         elapsed_sec = 0
         baselines["co2eq_base"] = sgp30.baseline_co2eq
@@ -57,4 +96,4 @@ while True:
         with open("data_file.json", "w") as outfile:
             json.dump(baselines, outfile)
 
-        print("**** Saved baselines to flash!")
+        logging.info("Stored baselines to flash.")
